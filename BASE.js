f45e5b15243ffb7065032648aa5c0bbfa3913a76
Kauan Fonseca <131304556+COWANBAS@users.noreply.github.com>
2025-01-17 17:51:31 -0300

    Add files via upload

---
+++ BASE.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name
+// @description
+// @namespace    Cowan
+// @license      CowBas
+// @version      1.0
+// @author       Cowanbas
+// @match        *://*/*
+// @run-at       document-start
+// ==/UserScript==
